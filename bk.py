4e60dadf9d9f0482536b08620f3df748622c6e7c
kdminh11o2 <84627551+kdminh11o2@users.noreply.github.com>
2025-05-14 00:25:38 +0700

    Update bk.py

--- bk.py
+++ bk.py
@@ -27,7 +27,7 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO,
     handlers=[
-        RotatingFileHandler('bot.log', maxBytes=10*1024*1024, backupCount=5),
+        RotatingFileHandler('bot.log', maxBytes=3*1024*1024, backupCount=3),
         logging.StreamHandler()
     ]
 )
